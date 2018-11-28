a6e35fee30b7df2dd721053bd005de60c983d58a
Liu Shuo A <shuo.a.liu@intel.com>
2018-11-28 18:06:08 +0800

    dm: pass-compiling: framebuffer
    Signed-off-by: Liu Shuo A <shuo.a.liu@intel.com>

--- devicemodel/include/rfb.h
+++ devicemodel/include/rfb.h
@@ -1,4 +1,6 @@
 /*-
+ * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+ *
  * Copyright (c) 2015 Tycho Nightingale <tycho.nightingale@pluribusnetworks.com>
  * All rights reserved.
  *
@@ -23,34 +25,14 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $FreeBSD$
+ * $FreeBSD: stable/11/usr.sbin/bhyve/rfb.h 336189 2018-07-11 07:16:13Z araujo $
  */
 
-#ifndef _CONSOLE_H_
-#define	_CONSOLE_H_
-#include <stdint.h>
+#ifndef _RFB_H_
+#define	_RFB_H_
 
-struct gfx_ctx;
+#define	RFB_PORT	5900
 
-typedef void (*fb_render_func_t)(struct gfx_ctx *gc, void *arg);
-typedef void (*kbd_event_func_t)(int down, uint32_t keysym, void *arg);
-typedef void (*ptr_event_func_t)(uint8_t mask, int x, int y, void *arg);
+int	rfb_init(char *hostname, int port, int wait, char *password);
 
-void	console_init(int w, int h, void *fbaddr);
-
-void	console_set_fbaddr(void *fbaddr);
-
-struct gfx_ctx_image *console_get_image(void);
-
-void	console_fb_register(fb_render_func_t render_cb, void *arg);
-void	console_refresh(void);
-
-void	console_kbd_register(kbd_event_func_t event_cb, void *arg, int pri);
-void	console_kbd_unregister(void);
-void	console_key_event(int down, uint32_t keysym);
-
-void	console_ptr_register(ptr_event_func_t event_cb, void *arg, int pri);
-void	console_ptr_unregister(void);
-void	console_ptr_event(uint8_t button, int x, int y);
-
-#endif /* _CONSOLE_H_ */
+#endif /* _RFB_H_ */
