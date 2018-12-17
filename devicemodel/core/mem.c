585fbc2ef3a742f8d0bde2e6dd12dc32d236f155
Liu Shuo A <shuo.a.liu@intel.com>
2018-12-17 15:05:05 +0800

    dm: fix deadlock between emulate_mem and un/regsiter_mem
    Signed-off-by: Wang Yu <yu.1.wang@intel.com>
    Signed-off-by: Chen Jian Jun<jian.jun.chen@intel.com>
    Signed-off-by: Liu Shuo A <shuo.a.liu@intel.com>

--- devicemodel/core/mem.c
+++ devicemodel/core/mem.c
@@ -184,8 +184,10 @@ emulate_mem(struct vmctx *ctx, struct mmio_request *mmio_req)
 	assert(entry != NULL);
 
 	if (entry->enabled == false) {
+		pthread_rwlock_unlock(&mmio_rwlock);
 		return -1;
 	}
+	pthread_rwlock_unlock(&mmio_rwlock);
 
 	if (mmio_req->direction == REQUEST_READ)
 		err = mem_read(ctx, 0, paddr, (uint64_t *)&mmio_req->value,
@@ -193,8 +195,6 @@ emulate_mem(struct vmctx *ctx, struct mmio_request *mmio_req)
 	else
 		err = mem_write(ctx, 0, paddr, mmio_req->value,
 				size, &entry->mr_param);
-
-	pthread_rwlock_unlock(&mmio_rwlock);
 
 	return err;
 }
