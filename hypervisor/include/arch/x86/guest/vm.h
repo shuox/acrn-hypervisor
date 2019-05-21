6e5297b28405509dc2bee07048dd4a32bfbb52e6
Shuo A Liu <shuo.a.liu@intel.com>
2019-05-21 14:08:18 +0800

    hv: Add active VMs traversing helper function
    Add a foreach_vm marco to traverse all active VMs.
    
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>
    Signed-off-by: Yu Wang <yu1.wang@intel.com>
    Signed-off-by: Shuo A Liu <shuo.a.liu@intel.com>

--- hypervisor/include/arch/x86/guest/vm.h
+++ hypervisor/include/arch/x86/guest/vm.h
@@ -148,6 +148,14 @@ struct acrn_vm {
 	uint64_t intr_inject_delay_delta; /* delay of intr injection */
 } __aligned(PAGE_SIZE);
 
+extern struct acrn_vm vm_array[CONFIG_MAX_VM_NUM];
+
+#define foreach_vm(idx, vm)			\
+	for ((idx) = 0U, (vm) = &vm_array[idx];	\
+		(idx) < CONFIG_MAX_VM_NUM;	\
+		(idx)++, (vm) = &vm_array[idx])	\
+		if (!is_poweroff_vm(vm))
+
 /*
  * @pre vlapic != NULL
  */
