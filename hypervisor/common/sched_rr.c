fcb14a613ce3d41583838456de5cc2b1c6ed0572
Shuo A Liu <shuo.a.liu@intel.com>
2019-06-25 15:57:18 +0800

    hv: sched_rr: add tick handler and runqueue operations
    sched_context is per-pcpu, each sched_context has a tick timer running
    periodically. Every period called a tick. In tick handler, we do
      1) replenish cycles to runnable sched_object who ran out of timeslice
      2) compute left timeslice of current sched_object if it's not the idle
      2) make a schedule request if current sched_object run out of timeslice
    
    For runqueue maintaining, we will keep objects which has timeslice in
    the first part of runqueue and the ones ran out of timeslice in the
    second part of runqueue.
    
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>
    Signed-off-by: Yu Wang <yu1.wang@intel.com>
    Signed-off-by: Shuo A Liu <shuo.a.liu@intel.com>

--- hypervisor/common/sched_rr.c
+++ hypervisor/common/sched_rr.c
@@ -18,8 +18,94 @@ struct sched_rr_data {
 	int64_t  left_cycles;
 };
 
-static void sched_tick_handler(__unused void *param)
+bool is_active(struct sched_object *obj)
 {
+	struct sched_rr_data *data = (struct sched_rr_data *)obj->data;
+	return !list_empty(&data->list);
+}
+
+void runqueue_add_head(struct sched_object *obj)
+{
+	struct sched_rr_context *rr_ctx = (struct sched_rr_context *)obj->ctx->priv;
+	struct sched_rr_data *data = (struct sched_rr_data *)obj->data;
+
+	if (!is_active(obj)) {
+		list_add(&data->list, &rr_ctx->runqueue);
+	}
+}
+
+void runqueue_add_tail(struct sched_object *obj)
+{
+	struct sched_rr_context *rr_ctx = (struct sched_rr_context *)obj->ctx->priv;
+	struct sched_rr_data *data = (struct sched_rr_data *)obj->data;
+	struct list_head *pos;
+	struct sched_object *iter;
+	struct sched_rr_data *iter_data;
+
+	if (!is_active(obj)) {
+		if (data->left_cycles <= 0) {
+			list_add_tail(&data->list, &rr_ctx->runqueue);
+		} else {
+			list_for_each(pos, &rr_ctx->runqueue) {
+				iter = list_entry(pos, struct sched_object, data);
+				iter_data = (struct sched_rr_data *)iter->data;
+				if (iter_data->left_cycles <= 0) {
+					break;
+				}
+			}
+			list_add_node(&data->list, pos->prev, pos);
+		}
+	}
+}
+
+void queue_remove(struct sched_object *obj)
+{
+	struct sched_rr_data *data = (struct sched_rr_data *)obj->data;
+	/* treat no queued object as paused, should wake it up when events arrive */
+	list_del_init(&data->list);
+}
+
+static void sched_tick_handler(void *param)
+{
+	struct sched_context *ctx = (struct sched_context *)param;
+	struct sched_rr_context *rr_ctx = (struct sched_rr_context *)ctx->priv;
+	struct sched_rr_data *data;
+	struct sched_object *current, *obj;
+	struct list_head *pos;
+	uint16_t pcpu_id = get_pcpu_id();
+	uint64_t now = rdtsc();
+
+	get_schedule_lock(pcpu_id);
+	current = ctx->current;
+
+	/* replenish sched_objects with slice_cycles, then reorder the runqueue according left_cycles */
+	list_for_each(pos, &rr_ctx->runqueue) {
+		obj = list_entry(pos, struct sched_object, data);
+		data = (struct sched_rr_data *)obj->data;
+		if (data->left_cycles > 0) {
+			continue;
+		}
+		data->left_cycles += data->slice_cycles;
+		queue_remove(obj);
+		runqueue_add_tail(obj);
+	}
+
+	/* If no vCPU start scheduling, ignore this tick */
+	if (current == NULL || (sched_is_idle(current) && list_empty(&rr_ctx->runqueue))) {
+		release_schedule_lock(pcpu_id);
+		return;
+	}
+	data = (struct sched_rr_data *)current->data;
+	/* consume the left_cycles of current sched_object if it is not idle */
+	if (!sched_is_idle(current)) {
+		data->left_cycles -= now - data->last_cycles;
+		data->last_cycles = now;
+	}
+	/* make reschedule request if current ran out of its cycles */
+	if (sched_is_idle(current) || data->left_cycles <= 0) {
+		make_reschedule_request(pcpu_id, DEL_MODE_IPI);
+	}
+	release_schedule_lock(pcpu_id);
 }
 
 int sched_rr_init(struct sched_context *ctx)
