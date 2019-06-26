22280538f3effe139a8cf030a13ba3cd0d249e11
Shuo A Liu <shuo.a.liu@intel.com>
2019-06-26 18:41:09 +0800

    hv: test: enable industry with cpu sharing
    Signed-off-by: Shuo A Liu <shuo.a.liu@intel.com>

--- hypervisor/scenarios/industry/vm_configurations.c
+++ hypervisor/scenarios/industry/vm_configurations.c
@@ -11,6 +11,7 @@ struct acrn_vm_config vm_configs[CONFIG_MAX_VM_NUM] = {
 	{
 		.load_order = SOS_VM,
 		.name = "ACRN SOS VM",
+		.scheduler = "sched_rr",
 		.uuid = {0xdbU, 0xbbU, 0xd4U, 0x34U, 0x7aU, 0x57U, 0x42U, 0x16U,	\
 			 0xa1U, 0x2cU, 0x22U, 0x01U, 0xf1U, 0xabU, 0x02U, 0x40U},
 			/* dbbbd434-7a57-4216-a12c-2201f1ab0240 */
@@ -38,6 +39,9 @@ struct acrn_vm_config vm_configs[CONFIG_MAX_VM_NUM] = {
 	},
 	{
 		.load_order = POST_LAUNCHED_VM,
+		.cpu_num = 4U,
+		.vcpu_affinity = {1U << 0U, 1U << 1U, 1U << 2U, 1U << 3U},
+		.scheduler = "sched_rr",
 		.uuid = {0xd2U, 0x79U, 0x54U, 0x38U, 0x25U, 0xd6U, 0x11U, 0xe8U,	\
 			 0x86U, 0x4eU, 0xcbU, 0x7aU, 0x18U, 0xb3U, 0x46U, 0x43U},
 			/* d2795438-25d6-11e8-864e-cb7a18b34643 */
@@ -53,6 +57,9 @@ struct acrn_vm_config vm_configs[CONFIG_MAX_VM_NUM] = {
 	},
 	{
 		.load_order = POST_LAUNCHED_VM,
+		.cpu_num = 2U,
+		.vcpu_affinity = {1U << 4U, 1U << 5U},
+		.scheduler = "sched_noop",
 		.uuid = {0x49U, 0x5aU, 0xe2U, 0xe5U, 0x26U, 0x03U, 0x4dU, 0x64U,	\
 			 0xafU, 0x76U, 0xd4U, 0xbcU, 0x5aU, 0x8eU, 0xc0U, 0xe5U},
 			/* 495ae2e5-2603-4d64-af76-d4bc5a8ec0e5 */
@@ -61,7 +68,8 @@ struct acrn_vm_config vm_configs[CONFIG_MAX_VM_NUM] = {
 		.guest_flags = GUEST_FLAG_HIGHEST_SEVERITY,
 		.vuart[0] = {
 			.type = VUART_LEGACY_PIO,
-			.addr.port_base = INVALID_COM_BASE,
+			.addr.port_base = CONFIG_COM_BASE,
+			.irq = CONFIG_COM_IRQ,
 		},
 		.vuart[1] = {
 			.type = VUART_LEGACY_PIO,
@@ -71,12 +79,16 @@ struct acrn_vm_config vm_configs[CONFIG_MAX_VM_NUM] = {
 	},
 	{
 		.load_order = POST_LAUNCHED_VM,
+		.cpu_num = 2U,
+		.vcpu_affinity = {1U << 6U, 1U << 7U},
+		.scheduler = "sched_noop",
 		.uuid = {0x38U, 0x15U, 0x88U, 0x21U, 0x52U, 0x08U, 0x40U, 0x05U,	\
 			 0xb7U, 0x2aU, 0x8aU, 0x60U, 0x9eU, 0x41U, 0x90U, 0xd0U},
 			/* 38158821-5208-4005-b72a-8a609e4190d0 */
 		.vuart[0] = {
 			.type = VUART_LEGACY_PIO,
-			.addr.port_base = INVALID_COM_BASE,
+			.addr.port_base = CONFIG_COM_BASE,
+			.irq = CONFIG_COM_IRQ,
 		},
 		.vuart[1] = {
 			.type = VUART_LEGACY_PIO,
