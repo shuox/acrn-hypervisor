24599c3aca233269b1d600c7b913e7189b9655ec
Shuo A Liu <shuo.a.liu@intel.com>
2019-07-01 18:17:39 +0800

    dm: scripts: Do not offline sos cpus for cpu sharing
    Signed-off-by: Shuo A Liu <shuo.a.liu@intel.com>

--- devicemodel/samples/apl-mrb/launch_uos.sh
+++ devicemodel/samples/apl-mrb/launch_uos.sh
@@ -555,22 +555,22 @@ if [ $runC_enable == 1 ]; then
 fi
 
 # offline SOS CPUs except BSP before launch UOS
-for i in `ls -d /sys/devices/system/cpu/cpu[1-99]`; do
-        online=`cat $i/online`
-        idx=`echo $i | tr -cd "[1-99]"`
-        echo cpu$idx online=$online
-        if [ "$online" = "1" ]; then
-                echo 0 > $i/online
-		online=`cat $i/online`
+#for i in `ls -d /sys/devices/system/cpu/cpu[1-99]`; do
+#        online=`cat $i/online`
+#        idx=`echo $i | tr -cd "[1-99]"`
+#        echo cpu$idx online=$online
+#        if [ "$online" = "1" ]; then
+#                echo 0 > $i/online
+#		online=`cat $i/online`
 		# during boot time, cpu hotplug may be disabled by pci_device_probe during a pci module insmod
-		while [ "$online" = "1" ]; do
-			sleep 1
-			echo 0 > $i/online
-			online=`cat $i/online`
-		done
-                echo $idx > ${offline_path}/offline_cpu
-        fi
-done
+#		while [ "$online" = "1" ]; do
+#			sleep 1
+#			echo 0 > $i/online
+#			online=`cat $i/online`
+#		done
+#                echo $idx > ${offline_path}/offline_cpu
+#        fi
+#done
 
 case $launch_type in
 	1) echo "Launch clearlinux UOS"
