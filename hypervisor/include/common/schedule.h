f4d2ca72633a265f8ae265672e42a5234096b173
Conghui Chen <conghui.chen@intel.com>
2019-09-04 19:39:54 +0000

    hv: sched: disable interrupt when grab schedule spinlock
    After moving softirq to following interrupt path, softirq handler might
    break in the schedule spinlock context and try to grab the lock again,
    then deadlock.
    
    Disable interrupt with schedule spinlock context.
    
    Signed-off-by: Conghui Chen <conghui.chen@intel.com>

--- hypervisor/include/common/schedule.h
+++ hypervisor/include/common/schedule.h
@@ -89,8 +89,8 @@ struct thread_object *sched_get_current(uint16_t pcpu_id);
 void init_sched(uint16_t pcpu_id);
 void deinit_sched(uint16_t pcpu_id);
 void switch_to_idle(thread_entry_t idle_thread);
-void get_schedule_lock(uint16_t pcpu_id);
-void release_schedule_lock(uint16_t pcpu_id);
+void get_schedule_lock(uint16_t pcpu_id, uint64_t *rflag);
+void release_schedule_lock(uint16_t pcpu_id, uint64_t rflag);
 
 void init_thread_data(struct thread_object *obj);
 void deinit_thread_data(struct thread_object *obj);
