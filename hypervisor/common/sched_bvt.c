ad8e4820b1ac70986f863b6d1525568b6a828090
Conghui Chen <conghui.chen@intel.com>
2020-02-06 16:02:10 +0000

    hv: sched_bvt: add init and init_data function
    Add init function for bvt scheduler, creating a runqueue and a period
    timer, the timer interval is default as 1ms. The interval is the minimum
    charging unit.
    Add structure sched_bvt_data, and init some members in it.
    
    Signed-off-by: Conghui Chen <conghui.chen@intel.com>

--- hypervisor/common/sched_bvt.c
+++ hypervisor/common/sched_bvt.c
@@ -8,17 +8,70 @@
 #include <per_cpu.h>
 #include <schedule.h>
 
-int sched_bvt_init(__unused struct sched_control *ctl)
+#define CONFIG_MCU_MS	1U
+#define CONFIG_CSA_MCU_NUM 5U
+struct sched_bvt_data {
+	/* keep list as the first item */
+	struct list_head list;
+
+	uint64_t mcu;
+	uint64_t mcu_ratio;
+	uint16_t weight;
+	/* context switch allowance */
+	uint64_t cs_allow_mcu;
+	int64_t run_mcu;
+	/* scheduler virtual time */
+	int64_t svt_mcu;
+	/* actual virtual time */
+	int64_t avt_mcu;
+	/* effective virtual time */
+	int64_t evt_mcu;
+
+	uint64_t start;
+};
+
+static void sched_tick_handler(__unused void *param)
 {
-	return 0;
+}
+
+int sched_bvt_init(struct sched_control *ctl)
+{
+	struct sched_bvt_control *bvt_ctl = &per_cpu(sched_bvt_ctl, ctl->pcpu_id);
+	uint64_t tick_period = CYCLES_PER_MS;
+	int ret = 0;
+
+	ASSERT(get_pcpu_id() == ctl->pcpu_id, "Init scheduler on wrong CPU!");
+
+	ctl->priv = bvt_ctl;
+	INIT_LIST_HEAD(&bvt_ctl->runqueue);
+
+	/* The tick_timer is periodically */
+	initialize_timer(&bvt_ctl->tick_timer, sched_tick_handler, ctl,
+			rdtsc() + tick_period, TICK_MODE_PERIODIC, tick_period);
+
+	if (add_timer(&bvt_ctl->tick_timer) < 0) {
+		pr_err("Failed to add schedule tick timer!");
+		ret = -1;
+	}
+
+	return ret;
 }
 
 void sched_bvt_deinit(__unused struct sched_control *ctl)
 {
 }
 
-void sched_bvt_init_data(__unused struct thread_object *obj)
+void sched_bvt_init_data(struct thread_object *obj)
 {
+	struct sched_bvt_data *data;
+
+	data = (struct sched_bvt_data *)obj->data;
+	INIT_LIST_HEAD(&data->list);
+	data->mcu = CONFIG_MCU_MS * CYCLES_PER_MS;
+	/* TODO: mcu advance value should be proportional to weight. */
+	data->mcu_ratio = 1;
+	data->cs_allow_mcu = CONFIG_CSA_MCU_NUM;
+	data->run_mcu = data->cs_allow_mcu;
 }
 
 static struct thread_object *sched_bvt_pick_next(__unused struct sched_control *ctl)
