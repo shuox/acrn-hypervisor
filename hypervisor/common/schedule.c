shuox/acrn-hypervisor

commit 2abbb99f6ac52552f21d69424f2adb2c3fdca25a

hv: make thread status more accurate
1. Update thread status after switch_in/switch_out.
2. Add 'be_blocking' to represent the intermediate state during
sleep_thread and switch_out. After switch_out, the thread status
update to THREAD_STS_BLOCKED.

Tracked-On: #5057
Signed-off-by: Conghui Chen <conghui.chen@intel.com>
Reviewed-by: Shuo A Liu <shuo.a.liu@intel.com>
Acked-by: Eddie Dong <eddie.dong@intel.com>

hypervisor/common/schedule.c
@@ -169,25 +169,26 @@ void schedule(void)
 	}
 	bitmap_clear_lock(NEED_RESCHEDULE, &ctl->flags);
 
-	/* Don't change prev object's status if it's not running */
-	if (is_running(prev)) {
-		set_thread_status(prev, THREAD_STS_RUNNABLE);
-	}
-	set_thread_status(next, THREAD_STS_RUNNING);
-	ctl->curr_obj = next;
-	release_schedule_lock(pcpu_id, rflag);
-
 	/* If we picked different sched object, switch context */
 	if (prev != next) {
-		if ((prev != NULL) && (prev->switch_out != NULL)) {
-			prev->switch_out(prev);
+		if (prev != NULL) {
+			if (prev->switch_out != NULL) {
+				prev->switch_out(prev);
+			}
+			set_thread_status(prev, prev->be_blocking ? THREAD_STS_BLOCKED : THREAD_STS_RUNNABLE);
+			prev->be_blocking = false;
 		}
 
-		if ((next != NULL) && (next->switch_in != NULL)) {
+		if (next->switch_in != NULL) {
 			next->switch_in(next);
 		}
+		set_thread_status(next, THREAD_STS_RUNNING);
 
+		ctl->curr_obj = next;
+		release_schedule_lock(pcpu_id, rflag);
 		arch_switch_to(&prev->host_sp, &next->host_sp);
+	} else {
+		release_schedule_lock(pcpu_id, rflag);
 	}
 }
 
@@ -208,7 +209,7 @@ void sleep_thread(struct thread_object *obj)
 			make_reschedule_request(pcpu_id, DEL_MODE_IPI);
 		}
 	}
-	set_thread_status(obj, THREAD_STS_BLOCKED);
+	obj->be_blocking = true;
 	release_schedule_lock(pcpu_id, rflag);
 }
 

hypervisor/include/common/schedule.h
@@ -37,6 +37,7 @@ struct thread_object {
 	struct sched_control *sched_ctl;
 	thread_entry_t thread_entry;
 	volatile enum thread_object_state status;
+	bool be_blocking;
 	enum sched_notify_mode notify_mode;
 
 	uint64_t host_sp;

commit dade0c4cd88a2043c4a872e0f612161c4748b07b

hv: Fix thread status mess if wake_thread() happens in transition stage
2abbb99f6ac5 ("hv: make thread status more accurate") introduced a
transition stage, marked as var be_blocking, between RUNNING->BLOCKED
of thread status. wake_thread() does not work in this transition stage
because it only checks thread->status.

Need to check thread->be_blocking as well in wake_thread(). When
wake_thread() happens in the transition stage, the previous sleep
operation rolled back.

Fixes: 2abbb99f6ac5 ("hv: make thread status more accurate")
Signed-off-by: Conghui Chen <conghui.chen@intel.com>
Signed-off-by: Shuo A Liu <shuo.a.liu@intel.com>

hypervisor/common/schedule.c
@@ -225,13 +225,16 @@ void wake_thread(struct thread_object *obj)
 	uint64_t rflag;
 
 	obtain_schedule_lock(pcpu_id, &rflag);
-	if (is_blocked(obj)) {
+	if (is_blocked(obj) || obj->be_blocking) {
 		scheduler = get_scheduler(pcpu_id);
 		if (scheduler->wake != NULL) {
 			scheduler->wake(obj);
 		}
-		set_thread_status(obj, THREAD_STS_RUNNABLE);
-		make_reschedule_request(pcpu_id, DEL_MODE_IPI);
+		if (is_blocked(obj)) {
+			set_thread_status(obj, THREAD_STS_RUNNABLE);
+			make_reschedule_request(pcpu_id, DEL_MODE_IPI);
+		}
+		obj->be_blocking = false;
 	}
 	release_schedule_lock(pcpu_id, rflag);
 }